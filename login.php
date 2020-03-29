642d591ed17023f6ee346aa60d7bb35214b5e144
Emad <sudani.emad@gmail.com>
2020-03-29 19:37:44 +0430

    rewrite with function

--- login.php
+++ login.php
@@ -1,13 +1,4 @@
 <?php
-    $file = $_FILES['upfile'];
-    $dir = "uploaded";
-    $from = $file['tmp_name'];
-    $to = "$dir/".$file['name'];
-    if (!file_exists("$dir")) {
-        mkdir("$dir");
-        echo "directory created successfully";
-    } else {
-        echo "directory is already exists";
-    }
-    move_uploaded_file($from, $to);
+    include 'Util.php';
+    upload("upfile");
 ?>
