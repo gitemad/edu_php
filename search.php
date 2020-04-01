2da2e9b930b22917a5cbb2dc411673cfca0dbab2
Emad <sudani.emad@gmail.com>
2020-04-01 22:16:08 +0430

    implement table to show users and also delete feature

--- search.php
+++ search.php
@@ -1,19 +1,3 @@
-<?php
-    if (isset($_GET['search'])) {
-        $text = $_GET['search'];
-        $connection = mysqli_connect("localhost", "root", "", "edu_db");
-        if (!$connection) {
-            die();
-        } else {
-            $query = "SELECT username, email, age FROM user_tbl WHERE username LIKE '%$text%' OR email LIKE '%$text%'";
-            $data = mysqli_query($connection, $query);
-            while ($record = mysqli_fetch_assoc($data)) {
-                var_dump($record);
-            }
-        }
-    }
-?>
-
 <html>
     <head>
     </head>
@@ -22,5 +6,40 @@
     		<input name="search" type="text">
     		<input name="submit" type="submit" value="Search">
     	</form>
+
+    <?php
+        if (isset($_GET['search'])) {
+            $text = $_GET['search'];
+            $connection = mysqli_connect("localhost", "root", "", "edu_db");
+            if (!$connection) {
+                die();
+            } else {
+                $query = "SELECT username, email, age, profile_pic FROM user_tbl WHERE username LIKE '%$text%' OR email LIKE '%$text%'";
+                $data = mysqli_query($connection, $query);
+                
+            echo '<table>';
+                echo '<tr>';
+                    echo '<th>username</th>';
+                    echo '<th>email</th>';
+                    echo '<th>age</th>';
+                    echo '<th>profile</th>';
+                    echo '<th>delete</th>';
+                echo '</tr>';
+                while ($record = mysqli_fetch_assoc($data)) {
+                    echo '<tr>';
+                        echo "<td>$record[username]</td>";
+                        echo "<td>$record[email]</td>";
+                        echo "<td>$record[age]</td>";
+                        echo "<td> <img src=$record[profile_pic]></td>";
+                        echo "<td> <a  href='delete.php?username=$record[username]&search=$text'> 
+                                        <img src='img/delete_icon.png'></td>
+                                    </a>";
+                    echo '</tr>';
+                }
+            echo '</table>';
+            }
+        }
+    ?>
+
 	</body>
 </html>
