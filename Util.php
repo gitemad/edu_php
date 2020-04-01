7e7a5f98024b2087bbf564a46ca1cd67ff1a822a
Emad <sudani.emad@gmail.com>
2020-04-01 21:17:08 +0430

    upload returns directory

--- Util.php
+++ Util.php
@@ -1,14 +1,17 @@
 <?php
-    function upload($uploadedfile, $dir="uploaded") {
-        $file = $_FILES[$uploadedfile];
+
+    function upload($tag_name, $dir="uploaded") {
+        $file = $_FILES[$tag_name];
         $from = $file['tmp_name'];
         $to = "$dir/".$file['name'];
         if (!file_exists("$dir")) {
             mkdir("$dir");
-            echo "directory created successfully";
+//             echo "directory created successfully";
         } else {
-            echo "directory is already exists";
+//             echo "directory already exists";
         }
         move_uploaded_file($from, $to);
+        return $to;
     }
+    
 ?>
