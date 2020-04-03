3121158115e52d4c692d12755495f796ac6669b6
gitemad <61329625+gitemad@users.noreply.github.com>
2020-04-04 03:49:29 +0430

    comment from github

--- create_db.php
+++ create_db.php
@@ -1,6 +1,6 @@
 <?php
     $con = mysqli_connect("localhost", 'root', '');
-    
+    //this is comment from github
     $query = "CREATE DATABASE db_created";
     
     mysqli_query($con, $query);
