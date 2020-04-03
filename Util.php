e6815c07fda030be60599e46c6204b052e00ef7d
Emad <sudani.emad@gmail.com>
2020-04-04 03:06:56 +0430

    upload method supports no file selection

--- Util.php
+++ Util.php
@@ -2,6 +2,9 @@
 
     function upload($tag_name, $dir="uploaded") {
         $file = $_FILES[$tag_name];
+        if ($file['name']=="") {
+            return FALSE;
+        }
         $from = $file['tmp_name'];
         $to = "$dir/".$file['name'];
         if (!file_exists("$dir")) {
