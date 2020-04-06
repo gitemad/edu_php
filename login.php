c4133758f4b17ee75a3a07d969166b57e2172ff5
Emad <sudani.emad@gmail.com>
2020-04-06 16:32:43 +0430

    login with cookie

--- login.php
+++ login.php
@@ -1,7 +1,9 @@
 <?php
-    if ($_POST) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+//     setcookie('username', $username, (time() - 24 * 60 * 60));
+//     setcookie('password', $password, (time() - 24 * 60 * 60));
+    if (isset($_COOKIE['username'])) {
+        $username = $_COOKIE['username'];
+        $password = $_COOKIE['password'];
         
         $link = mysqli_connect("localhost", "root", "", "edu_db");
         $query = "SELECT * FROM user_tbl WHERE username='$username'";
@@ -11,6 +13,33 @@
             if (sha1($password) == $record['password']) {
                 session_start();
                 $_SESSION['username'] = $username;                
+                echo "Login with cookie";
+            } else {
+                echo "Incorrect Password!";
+            }
+        } else {
+            echo "User not found";
+        }
+    }
+    if ($_POST) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $remember = $_POST['remember'];
+        
+        $link = mysqli_connect("localhost", "root", "", "edu_db");
+        $query = "SELECT * FROM user_tbl WHERE username='$username'";
+        $data = mysqli_query($link, $query);
+        
+        if ($record = mysqli_fetch_array($data)) {
+            if (sha1($password) == $record['password']) {
+                session_start();
+                $_SESSION['username'] = $username; 
+                
+                if ($remember) {
+                    setcookie('username', $username, (time() + 24 * 60 * 60));
+                    setcookie('password', $password, (time() + 24 * 60 * 60));
+                }
+                
                 echo "Login OK";
             } else {
                 echo "Incorrect Password!";
