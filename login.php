21fdcaa3ae1f84641c1b3920d6f80ad0467b1a19
Emad <sudani.emad@gmail.com>
2020-04-07 18:24:27 +0430

    comment cookies

--- login.php
+++ login.php
@@ -1,26 +1,26 @@
 <?php
 //     setcookie('username', $username, (time() - 24 * 60 * 60));
 //     setcookie('password', $password, (time() - 24 * 60 * 60));
-    if (isset($_COOKIE['username'])) {
-        $username = $_COOKIE['username'];
-        $password = $_COOKIE['password'];
+//     if (isset($_COOKIE['username'])) {
+//         $username = $_COOKIE['username'];
+//         $password = $_COOKIE['password'];
         
-        $link = mysqli_connect("localhost", "root", "", "edu_db");
-        $query = "SELECT * FROM user_tbl WHERE username='$username'";
-        $data = mysqli_query($link, $query);
+//         $link = mysqli_connect("localhost", "root", "", "edu_db");
+//         $query = "SELECT * FROM user_tbl WHERE username='$username'";
+//         $data = mysqli_query($link, $query);
         
-        if ($record = mysqli_fetch_array($data)) {
-            if (sha1($password) == $record['password']) {
-                session_start();
-                $_SESSION['username'] = $username;                
-                echo "Login with cookie";
-            } else {
-                echo "Incorrect Password!";
-            }
-        } else {
-            echo "User not found";
-        }
-    }
+//         if ($record = mysqli_fetch_array($data)) {
+//             if (sha1($password) == $record['password']) {
+//                 session_start();
+//                 $_SESSION['username'] = $username;                
+//                 echo "Login with cookie";
+//             } else {
+//                 echo "Incorrect Password!";
+//             }
+//         } else {
+//             echo "User not found";
+//         }
+//     }
     if ($_POST) {
         $username = $_POST['username'];
         $password = $_POST['password'];
@@ -40,11 +40,14 @@
                     setcookie('password', $password, (time() + 24 * 60 * 60));
                 }
                 
-                echo "Login OK";
+                header('location: userdetail.php');
+                
             } else {
+                header('location: login.php');
                 echo "Incorrect Password!";
             }
         } else {
+            header('location: login.php');
             echo "User not found";
         }
         
